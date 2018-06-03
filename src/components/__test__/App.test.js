d2ab91fc01d0657bd9078bc3253424ecf1812ca3
Nicolas Riquelme Guzman <nrriquelme@gmail.com>
2018-06-03 19:05:56 -0400

    more setup for basic testing

--- src/components/__test__/App.test.js
+++ src/components/__test__/App.test.js
@@ -1,7 +1,21 @@
 import React from 'react'
-import App from '../App'
+import App from 'components/App'
 import { shallow } from 'enzyme'
 
-it('renders without crashing', () => {
-  
+import CommentBox from 'components/CommentBox'
+import CommentList from 'components/CommentList';
+
+let wrapped
+
+beforeEach(() => {
+  wrapped = shallow(<App />)
+})
+
+it('show instacen of CommentBox and CommentList', () => {
+  // Making the assertion => then the assertion 
+  expect(wrapped.find(CommentBox).length).toEqual(1)
 });
+
+it('show an instance of CommentList', () => {
+  expect(wrapped.find(CommentList).length).toEqual(1)
+})
