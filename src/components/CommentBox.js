bc2d2111b67bb0d19b2184668a59b647e46be7f7
Nicolas Riquelme Guzman <nrriquelme@gmail.com>
2018-06-03 19:12:30 -0400

    adding the form for the comments and event handlers

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,5 +1,34 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-export default () => {
-  return <div>Comment Box</div>
+export class CommentBox extends Component {
+  state = {
+    comment: ''
+  }
+  handleChange = e => {
+    this.setState({ comment: e.target.value })
+  }
+  handleSubmit = e => {
+    e.preventDefault()
+
+    // call and action creator
+    // and save the comment
+    this.setState({
+      comment: ''
+    })
+  }
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <h4>Add a Comment</h4>
+          <textarea  value={this.state.comment} onChange={this.handleChange}/>
+          <div>
+            <button>Submit Comment</button>
+          </div>
+        </form>
+      </div>
+    )
+  }
 }
+
+export default CommentBox
